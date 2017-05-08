ba1dbba8a82b336b99c1006f28b330c545771d6f
Steve Wise <steven.wise@hmcts.net>
2017-05-08 10:52:09 +0100

    Updated touting for BS10

--- app/routes.js
+++ app/routes.js
@@ -9,7 +9,7 @@ var prototypeFourCtrl = require('./controllers/prototype-4');
 var prototypeFiveCtrl = require('./controllers/prototype-5');
 var prototypeSixCtrl = require('./controllers/prototype-6');
 var prototypeSevenCtrl = require('./controllers/prototype-7');
-var prototypeEightCtrl = require('./controllers/prototype-8');
+var prototypeEightCtrl = require('./controllers/prototype-10');
 
 router.get('/', home.index);
 router.get('/prototype-1/status', prototypeOneCtrl.status);
@@ -32,15 +32,15 @@ router.get('/prototype-6/claimexpenses', prototypeSixCtrl.claimExpenses);
 router.get('/prototype-7/validate',   prototypeSevenCtrl.validateCaseReference);
 router.get('/prototype-7/validatemulti',   prototypeSevenCtrl.validateCaseReferenceMulti);
 
-router.get('/prototype-8/validate',   prototypeEightCtrl.validateCaseReference);
-router.get('/prototype-8/validatemulti',   prototypeEightCtrl.validateCaseReferenceMulti);
-router.get('/prototype-8/validatereference',   prototypeEightCtrl.validateReference);
-router.get('/prototype-8/validate-surname',   prototypeEightCtrl.validateSurname);
-router.get('/prototype-8/status',   prototypeEightCtrl.getAppealStatus);
-router.post('/prototype-8/appointee',   prototypeEightCtrl.appointee);
-router.post('/prototype-8/representative',   prototypeEightCtrl.representative);
-router.post('/prototype-8/supporter',   prototypeEightCtrl.supporter);
-router.post('/prototype-8/hearing',   prototypeEightCtrl.hearing);
-router.post('/prototype-8/arrangements',   prototypeEightCtrl.arrangements);
+router.get('/prototype-10/validate',   prototypeEightCtrl.validateCaseReference);
+router.get('/prototype-10/validatemulti',   prototypeEightCtrl.validateCaseReferenceMulti);
+router.get('/prototype-10/validatereference',   prototypeEightCtrl.validateReference);
+router.get('/prototype-10/validate-surname',   prototypeEightCtrl.validateSurname);
+router.get('/prototype-10/status',   prototypeEightCtrl.getAppealStatus);
+router.post('/prototype-10/appointee',   prototypeEightCtrl.appointee);
+router.post('/prototype-10/representative',   prototypeEightCtrl.representative);
+router.post('/prototype-10/supporter',   prototypeEightCtrl.supporter);
+router.post('/prototype-10/hearing',   prototypeEightCtrl.hearing);
+router.post('/prototype-10/arrangements',   prototypeEightCtrl.arrangements);
 
 module.exports = router;
