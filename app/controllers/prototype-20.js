686a4596563061ab086cb17db24824d0b04fcd21
Steve Wise <steven.wise@hmcts.net>
2017-10-04 07:13:46 +0100

    non-js date picker added to appointee flow

--- app/controllers/prototype-20.js
+++ app/controllers/prototype-20.js
@@ -64,7 +64,7 @@ var controller = {
 
     availability: function(req, res) {
         if (req.body.radioGroup === 'datesYes') {
-            res.render('prototype-beta-20/submit-your-appeal/016-hearing-dates');
+            res.render('prototype-beta-20/submit-your-appeal/016-hearing-dates-nonjs');
         } else if (req.body.radioGroup === 'datesNo') {
             res.render('prototype-beta-20/submit-your-appeal/017-check-your-answers');
         }
@@ -72,7 +72,7 @@ var controller = {
 
     availabilityApp: function(req, res) {
         if (req.body.radioGroup === 'datesYesApp') {
-            res.render('prototype-beta-20/submit-your-appeal/016-hearing-dates-appointee');
+            res.render('prototype-beta-20/submit-your-appeal/016-hearing-dates-appointee-nonjs');
         } else if (req.body.radioGroup === 'datesNoApp') {
             res.render('prototype-beta-20/submit-your-appeal/017-check-your-answers-appointee');
         }
