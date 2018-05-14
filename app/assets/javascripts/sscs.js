7a859421fcb4823d78bcb062f93c6dc74278adfc
bendiggle <ben.diggle5@gmail.com>
2018-05-14 22:31:03 +0100

    put tabs JS into function and call function when doc ready. Add tab styling

--- app/assets/javascripts/sscs.js
+++ app/assets/javascripts/sscs.js
@@ -21,47 +21,48 @@ function ShowHideUpdates () {
     }
 }
 
-$(document).ready(function () {
-    var showHideUpdates =  new ShowHideUpdates();
-    showHideUpdates.toggleUpdates();
-});
-
-
-
-// Tabs
+function tabs() {
+  // Tabs
 
 // as JS is working, add 'tabs' class, which will style tabs and allow
 // functionality
-$('.js-tabs').addClass('tabs');
+  $('.js-tabs').addClass('tabs');
 
 // hide all of the tab content for now
-$('.tab-content').hide();
+  $('.tab-content').hide();
 //show the first tab and content
-$('.tabs').each(function(){
-  $(this).find('.tab-content:first').show();
-  $(this).find('ul li:first').addClass('active');
-});
+  $('.tabs').each(function(){
+    $(this).find('.tab-content:first').show();
+    $(this).find('ul li:first').addClass('active');
+  });
 
 // click function for tabs
-$('.tabs__link').click(function(e){
-  e.preventDefault();
+  $('.tabs__link').click(function(e){
+    e.preventDefault();
 
-  var tabs = $(this).parents('.tabs');
-  var link = $(this);
-  var currentTab = link.attr('href');
+    var tabs = $(this).parents('.tabs');
+    var link = $(this);
+    var currentTab = link.attr('href');
 
-  // remove active class from nav and add to newly selected tab
-  tabs.find('li').removeClass('active');
-  link.parent('li').addClass('active');
+    // remove active class from nav and add to newly selected tab
+    tabs.find('li').removeClass('active');
+    link.parent('li').addClass('active');
 
-  // hide all of the tab content and show newly selected then update hash in URL
-  tabs.find('.tab-content').hide();
-  $(currentTab).show();
-  history.pushState({}, '', currentTab);
-});
+    // hide all of the tab content and show newly selected then update hash in URL
+    tabs.find('.tab-content').hide();
+    $(currentTab).show();
+    history.pushState({}, '', currentTab);
+  });
 
 // check for hash in url and open that tab if its there
-var hash = window.location.hash;
-if (hash) {
-  $('.tabs__link[href="' + hash +'"]').click();
+  var hash = window.location.hash;
+  if (hash) {
+    $('.tabs__link[href="' + hash +'"]').click();
+  }
 }
+
+$(document).ready(function () {
+    var showHideUpdates =  new ShowHideUpdates();
+    showHideUpdates.toggleUpdates();
+    tabs();
+});
